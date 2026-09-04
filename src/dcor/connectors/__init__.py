d4cff85f1e5c7090a94077eacf82d1b9d71e1299
vortex <sobrinhosj@gmail.com>
2026-09-04 17:03:59 -0300

    feat(s2): expose connector sdk public interface

---
+++ src/dcor/connectors/__init__.py
@@ -0,0 +1,3 @@
+from .sdk import AuthenticationError, Connector, ConnectorError, ConnectorHealth, NormalizationError
+
+__all__ = ["AuthenticationError", "Connector", "ConnectorError", "ConnectorHealth", "NormalizationError"]
